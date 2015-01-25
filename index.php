5774b4b7832e521ccb783e5a7e5f40fcd7249266
jobayerccj <jobayerccj@gmail.com>
2015-01-25 22:05:41 +0600

    esc_url() function added

--- index.php
+++ index.php
@@ -31,38 +31,38 @@
 						<h2>Recent Work</h2>
 						<div class="row">
 							<article class="6u 12u$(xsmall) work-item">
-                                                            <a href="<?php echo get_bloginfo('template_directory');?>/images/fulls/01.jpg" class="image fit thumb">
-                                                                <img src="<?php echo get_bloginfo('template_directory');?>/images/thumbs/01.jpg" alt="" /></a>
+                                                            <a href="<?php echo esc_url( get_template_directory_uri() )?>/images/fulls/01.jpg" class="image fit thumb">
+                                                                <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/thumbs/01.jpg" alt="" /></a>
 								<h3>Magna sed consequat tempus</h3>
 								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
 							</article>
 							<article class="6u$ 12u$(xsmall) work-item">
-								<a href="<?php echo get_bloginfo('template_directory');?>/images/fulls/02.jpg" class="image fit thumb">
-                                                                    <img src="<?php echo get_bloginfo('template_directory');?>/images/thumbs/02.jpg" alt="" /></a>
+								<a href="<?php echo esc_url( get_template_directory_uri() )?>/images/fulls/02.jpg" class="image fit thumb">
+                                                                    <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/thumbs/02.jpg" alt="" /></a>
 								<h3>Ultricies lacinia interdum</h3>
 								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
 							</article>
 							<article class="6u 12u$(xsmall) work-item">
-								<a href="<?php echo get_bloginfo('template_directory');?>/images/fulls/03.jpg" class="image fit thumb">
-                                                                    <img src="<?php echo get_bloginfo('template_directory');?>/images/thumbs/03.jpg" alt="" /></a>
+								<a href="<?php echo esc_url( get_template_directory_uri() )?>/images/fulls/03.jpg" class="image fit thumb">
+                                                                    <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/thumbs/03.jpg" alt="" /></a>
 								<h3>Tortor metus commodo</h3>
 								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
 							</article>
 							<article class="6u$ 12u$(xsmall) work-item">
-								<a href="<?php echo get_bloginfo('template_directory');?>/images/fulls/04.jpg" class="image fit thumb">
-                                                                    <img src="<?php echo get_bloginfo('template_directory');?>/images/thumbs/04.jpg" alt="" /></a>
+								<a href="<?php echo esc_url( get_template_directory_uri() )?>/images/fulls/04.jpg" class="image fit thumb">
+                                                                    <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/thumbs/04.jpg" alt="" /></a>
 								<h3>Quam neque phasellus</h3>
 								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
 							</article>
 							<article class="6u 12u$(xsmall) work-item">
-								<a href="<?php echo get_bloginfo('template_directory');?>/images/fulls/05.jpg" class="image fit thumb">
-                                                                    <img src="<?php echo get_bloginfo('template_directory');?>/images/thumbs/05.jpg" alt="" /></a>
+								<a href="<?php echo esc_url( get_template_directory_uri() )?>/images/fulls/05.jpg" class="image fit thumb">
+                                                                    <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/thumbs/05.jpg" alt="" /></a>
 								<h3>Nunc enim commodo aliquet</h3>
 								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
 							</article>
 							<article class="6u$ 12u$(xsmall) work-item">
-								<a href="<?php echo get_bloginfo('template_directory');?>/images/fulls/06.jpg" class="image fit thumb">
-                                                                    <img src="<?php echo get_bloginfo('template_directory');?>/images/thumbs/06.jpg" alt="" /></a>
+								<a href="<?php echo esc_url( get_template_directory_uri() )?>/images/fulls/06.jpg" class="image fit thumb">
+                                                                    <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/thumbs/06.jpg" alt="" /></a>
 								<h3>Risus ornare lacinia</h3>
 								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
 							</article>
